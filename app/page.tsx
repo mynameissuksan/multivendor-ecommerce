b391198589076813d1e1b1344746bedfec0fcb62
aimnoii555 <aimnoii555@gmail.com>
2025-12-21 20:03:02 +0700

    shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 
-const page = () => {
+const Home = () => {
   return (
-    <div>
-      <h1 className="font-bold text-blue-500">Welcome to ecommerce</h1>
+    <div className="p-5">
+      <div className="w-full flex justify-end">
+        <ThemeToggle />
+      </div>
+      <h1 className="font-bold text-blue-500 font-barlow">
+        Welcome to ecommerce
+      </h1>
+      <Button>Click me</Button>
     </div>
   );
 };
 
-export default page;
+export default Home;
---
+++ components/shared/theme-toggle.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable react-hooks/set-state-in-effect */
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+const ThemeToggle = () => {
+  const { setTheme } = useTheme();
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="relative h-10 w-10 rounded-full"
+        >
+          <SunIcon className="h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle Theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-24">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ThemeToggle;
